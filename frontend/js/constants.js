42b9ff76581c5d822244bdbd2613ebb568db5b1a
Scott Shields <scottglshields@gmail.com>
2022-03-28 20:27:45 -0400

    u

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0x810Fa149796C9151214F00df29d0F26F73Abf481"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "";//"Welcome to the Narwhal Fight Club NFT Project!";
+const welcome_h1 = "Welcome to the Narwhal Fight Club NFT Project!";
 const welcome_h2 = "Connect to MetaMask to get started 👆🏼";
 const welcome_p = "Every year, narwhals gather near and far for an occasion of epic proportion. A club so magnificent that we shall not speak its name! They bring everything they have of value to the gathering and submit their name for a chance to be the NOM (Narwhal Ocean Master).";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!";
